1e3037897ccc59484652c6991415ab8e1bbf7003
Rahul A Mistry <rahulmistry.sde@gmail.com>
2025-11-01 14:49:58 +0530

    feat: initial commit

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+  printf("Hello, World!\n");
+
+  // print the number from 1 to 10
+  int n = 10;
+  for(int i = 1; i <= n; i++) {
+    printf("%d\n", i);
+  }
+}
